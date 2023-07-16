6bed1262121ea38891dba8d1beff27c7cb463ce6
Luan Rodrigues <luanrodrigues007@hotmail.com>
2023-07-16 05:36:15 -0300

    refactor: add rule shortcut

--- string_validator.go
+++ string_validator.go
@@ -6,6 +6,7 @@ type stringMessageFields string
 
 const (
 	Type        = stringMessageFields("type")
+	Value       = "Value"
 	ValueLen    = "ValueLne"
 	ExpectedLen = "ExpectedLne"
 )
@@ -31,30 +32,14 @@ func (v *StringValidator) Len(exactLen int) *StringValidator {
 
 // Lenf is the same as `len` validator, with addition of custom message when it fails.
 func (v *StringValidator) Lenf(exactLen int, format string, fields ...stringMessageFields) *StringValidator {
+	config := v.getDefaultStringMessagesConfig()
+	config[ExpectedLen] = exactLen
+
 	isValid := func(value string) bool {
 		return len(value) == exactLen
 	}
 
-	config := v.getDefaultStringMessagesConfig()
-	config[ExpectedLen] = exactLen
-
-	v.AddRule(func(value string) (bool, ValidationError[string]) {
-		config[ValueLen] = len(value)
-		err := ValidationError[string]{}
-
-		valid := isValid(value)
-
-		if !valid {
-			errMessageFields := v.buildFieldsValues(config, fields...)
-
-			err = ValidationError[string]{
-				message: fmt.Sprintf(format, errMessageFields...),
-				value:   value,
-			}
-		}
-
-		return valid, err
-	})
+	v.addRule(isValid, format, config, fields...)
 
 	return v
 }
@@ -77,4 +62,31 @@ func (v *StringValidator) buildFieldsValues(config map[stringMessageFields]any, 
 		}
 	}
 	return values
+}
+
+func (v *StringValidator) addRule(
+	isValid func(string) bool,
+	format string,
+	config map[stringMessageFields]any,
+	fields ...stringMessageFields,
+) {
+	v.Validator.AddRule(func(value string) (bool, ValidationError[string]) {
+		config[Value] = value
+		config[ValueLen] = len(value)
+
+		err := ValidationError[string]{}
+
+		valid := isValid(value)
+
+		if !valid {
+			errMessageFields := v.buildFieldsValues(config, fields...)
+
+			err = ValidationError[string]{
+				message: fmt.Sprintf(format, errMessageFields...),
+				value:   value,
+			}
+		}
+
+		return valid, err
+	})
 }
