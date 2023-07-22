76e23f8c7326d596aedc0e0302406694d0f7cf63
Luan Rodrigues <luanrodrigues007@hotmail.com>
2023-07-22 02:15:32 -0300

    test: add message field unique test

---
+++ messages_test.go
@@ -0,0 +1,27 @@
+package fluffy_validator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMessageFieldsAreUnique(t *testing.T) {
+	allMessageFields := []messageFields{
+		Type, Value, ValueLen, ExpectedLen,
+	}
+
+	set := make(map[messageFields]bool)
+
+	for _, field := range allMessageFields {
+		set[field] = true
+	}
+
+	assert.Len(
+		t,
+		set,
+		len(allMessageFields),
+		"There are message fields duplicated. Contains %d message fields, but only %d are unique",
+		len(allMessageFields),
+		len(set),
+	)
+}
